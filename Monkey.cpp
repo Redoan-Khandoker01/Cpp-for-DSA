c2a9ed53a1ba694f485cbddee07ab6c8f1b64e8d
Redoan Khandoker <redoankhandoker8066@gmail.com>
2025-11-21 15:31:42 +0600

    c++

--- Monkey.cpp
+++ Monkey.cpp
@@ -1,8 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main()
-{
+{   
+    string s;
 
+    while(getline(cin,s))
+    {   
+        string tem;
+        for(char c:s)
+        {
+            if(c !=' ')
+            {
+                tem += c;
+            }
+        }
+           
+
+
+
+        sort(tem.begin(),tem.end());
+       
+        cout<<tem<<"\n";
+
+    }
+
+    
+    
 
 
 return 0;
