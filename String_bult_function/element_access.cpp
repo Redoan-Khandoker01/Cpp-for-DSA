506a3ebc25445a1ae59cf475faf43bfa0b163341
Redoan Khandoker <redoankhandoker8066@gmail.com>
2025-11-22 16:11:09 +0600

    c++

---
+++ String_bult_function/element_access.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    cout<<s[2]<<endl;                 // string  একসেস নেয়া যায়. index[0];
+    cout<<s.back()<<endl;       // srtring er  লাস্ট এর  idex print korar jonno ;
+    cout<<s.front()<<endl;      // string er font index print korar jonno ;
+
+
+
+return 0;
+}
