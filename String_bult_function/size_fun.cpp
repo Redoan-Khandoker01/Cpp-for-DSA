29683d57bed91b8c0a8ee34a0ef1b5e1873377de
Redoan Khandoker <redoankhandoker8066@gmail.com>
2025-11-22 16:10:41 +0600

    c++

---
+++ String_bult_function/size_fun.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+    string s= "Redoan Khandker";
+    //..................................
+    // s.clear();                   // string মুছে ফেলে।
+     s.resize(18);                // size  ছোট বড় করার ফাংশন। 
+    cout<<s.size()<<endl;           // size  বের করার ফাংশন।
+    cout<<s<<endl;
+    cout<<s.capacity()<<endl;       // capacity  চেক করার ফাংশান ।
+
+    
+
+return 0;
+}
---
+++ String_bult_function/string_built_in.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s ="hello";
+    string s2 ="redoa";
+    if(s==s2)
+    {
+        cout<<"same";
+    }
+    else{
+        cout<<"not same";
+    }
+    
+
+
+
+
+
+return 0;
+}
