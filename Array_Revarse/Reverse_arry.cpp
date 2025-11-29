e55e7eab1a8875ebf02b96096516b1bc93052967
Redoan Khandoker <redoankhandoker8066@gmail.com>
2025-11-29 21:48:36 +0600

    redo

--- Array_Revarse/Reverse_arry.cpp
+++ Array_Revarse/Reverse_arry.cpp
@@ -14,13 +14,7 @@ reverse(a,a+n);
 for (int i = 0; i < n; i++)
 {
     cout<<a[i]<<" ";
-    cout<<a[i]<<" ";
-    cout<<a[i]<<" ";
-    cout<<a[i]<<" ";
-    cout<<a[i]<<" ";
-    cout<<a[i]<<" ";
-    cout<<a[i]<<" ";
-    cout<<a[i]<<" ";
+     cout<<a[i]<<" ";
 }
 
 
---
+++ more_about_class_and_string/range_base_for_loop.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    string s;
+    cin>>s;
+    for(char c:s)           //প্রতিটি ক‌্যারেক্টর আলাদার প্রিন্ট করে দেয় 
+    {
+        cout<<c<<endl;
+
+    }
+
+return 0;
+}
